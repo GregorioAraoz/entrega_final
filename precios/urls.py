aa2c672b1a5d4d6844733af9ea6c18f3860d635e
GregorioAraoz <garaozdelamadrid@gmail.com>
2025-08-10 23:12:42 -0300

    Boton de Agregar Alimento Creado

--- precios/urls.py
+++ precios/urls.py
@@ -1,12 +1,12 @@
 # precios/urls.py
 from django.urls import path
-from . import views
+from precios import views
 
 app_name = 'precios'
 
 urlpatterns = [
     path('', views.Alimentos.as_view(), name='lista_precios'),         
-    # path('crear/', views.crear_alimento, name='crear_alimento'),
+    path('crear/', views.CrearAlimento.as_view(), name='crear_alimento'),
     # path('<int:id>/', views.ver_alimento, name='ver_alimento'),
     # path('<int:id>/eliminar/', views.eliminar_alimento, name='eliminar_alimento'),
     # path('<int:id>/actualizar/', views.actualizar_alimento, name='actualizar_alimento'),
--- precios/views.py
+++ precios/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
 from precios.models import Alimento  
+from django.views.generic.edit import CreateView
 
 def alimento(request):
     return HttpResponse("Lista de precios está Funcionando")
@@ -9,3 +10,7 @@ def alimento(request):
 class Alimentos(ListView):
     model = Alimento
     template_name = "lista_precios.html"
+
+class CrearAlimento(CreateView):
+    model = Alimento
+    template_name = "crear_alimento.html"
